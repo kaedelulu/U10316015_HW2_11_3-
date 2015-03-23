5dbb5ce4091c0557db0e1b5bae2b9278dd3193a6
kaedelulu <luinghfrhrfuiy@gmail.com>
2015-03-23 21:40:00 +0800

    information
    a class only or checking information

---
+++ CheckingAccount.java
@@ -0,0 +1,8 @@
+CheckingAccount extends SavingsAccount{
+	public static void main(String[] args) {
+		//Create toString() method and Override
+		public String toString(){
+			return super.toString() + "\n" ;
+		}
+}
+
